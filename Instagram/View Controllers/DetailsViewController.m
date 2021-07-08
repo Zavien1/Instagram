d7aab96bfc858cbcef520c0e495273230e61db5f
Zavien Sibilia <zavien@fb.com>
2021-07-07 17:33:47 -0700

    add timestamp for posts

--- Instagram/View Controllers/DetailsViewController.m
+++ Instagram/View Controllers/DetailsViewController.m
@@ -8,11 +8,13 @@
 #import "DetailsViewController.h"
 #import "UIImageView+AFNetworking.h"
 #import "Parse/Parse.h"
+#import "DateTools.h"
 
 @interface DetailsViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *postImage;
 @property (weak, nonatomic) IBOutlet UILabel *postTextLabel;
 @property (weak, nonatomic) IBOutlet UILabel *postUserLabel;
+@property (weak, nonatomic) IBOutlet UILabel *postTimeLabel;
 
 @end
 
@@ -21,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.postTextLabel.text = self.post[@"text"];
+    self.postTimeLabel.text = [self.post createdAt].shortTimeAgoSinceNow;
     
     PFFileObject *image = self.post[@"image"];
     [self.postImage setImageWithURL:[NSURL URLWithString:image.url]];
